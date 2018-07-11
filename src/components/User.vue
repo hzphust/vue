213c682a77f37fd4c146e1e39c1b66306b2e8998
hzphust <hzphsut@yeah.net>
2018-07-12 01:21:23 +0800

    first commit

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  data () {
+    return {
+      msg: 'User location'
+    }
+  }
+}
+</script>
+
